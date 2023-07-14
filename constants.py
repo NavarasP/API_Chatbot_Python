a63bcd8c231d3a9611a369f84114d65f8261bc9e
Navaras P <69687392+NavarasP@users.noreply.github.com>
2023-07-15 01:53:06 +0530

    committ

--- constants.py
+++
@@ -1,3 +0,0 @@
-# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
-# and rename this file to constants.py.
-APIKEY = "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"
